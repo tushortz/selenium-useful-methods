e497e3f6006227137a22484302a2a3a1b4fed75f
Taiwo Kareem <taiwo.kareem36@gmail.com>
2018-07-06 10:46:29 +0100

    Update page_elements.py

--- page_elements.py
+++ page_elements.py
@@ -9,7 +9,12 @@ driver = webdriver.Chrome(chrome_options=co)
 def wait_for_element(selector_tuple, parent=driver, timeout=10, many=False):
     """
     Wait 10 seconds until element is visible in page then returns element
-    selector_tuple: required and needs to be a tuple e.g. (By.ID, "some-id")
+    
+    args:
+        selector_tuple: required and needs to be a tuple e.g. (By.ID, "some-id")
+        parent: optional. the default is the driver but can also be a web element(s)
+        timeout: optional argument which defaults to 10 seconds.
+        many: optional. defaults to False and returns a single web element. Change to True to return a list of web elements
     """
     if many:
         selector = EC.presence_of_all_elements_located(selector_tuple)
@@ -20,3 +25,4 @@ def wait_for_element(selector_tuple, parent=driver, timeout=10, many=False):
     return element
     
 # Example usage
+# wait_for_element((By.ID, "username"))
